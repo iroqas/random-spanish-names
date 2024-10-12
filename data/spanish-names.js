38c197ad4c4ec12e0beca0a08a2d1937ec0e1bbc
Iroqas <cemape@protonmail.com>
2024-10-12 21:22:29 +0200

    package.json updated

---
+++ data/spanish-names.js
@@ -0,0 +1,44 @@
+const maleNames = [
+    "Adolfo", "Adrián", "Agustín", "Alberto", "Alejandro", "Alonso", "Andrés", "Antonio", "Armando", "Arturo",
+    "Baltasar", "Benjamín", "Bernardo", "Bruno", "Carlos", "Casimiro", "Cesar", "Cristian", "Daniel", "Damián",
+    "David", "Diego", "Domingo", "Eduardo", "Emilio", "Enrique", "Ernesto", "Esteban", "Eugenio", "Fabián",
+    "Fausto", "Felipe", "Fernando", "Francisco", "Gabriel", "Gaspar", "Germán", "Gonzalo", "Guido", "Guillermo",
+    "Héctor", "Horacio", "Hugo", "Ignacio", "Ismael", "Iván", "Jacobo", "Jaime", "Jano", "Javier",
+    "Jesús", "Joaquín", "Jorge", "Josué", "José", "Juan", "Julián", "Leandro", "Leonardo", "Lisandro",
+    "Lorenzo", "Lucas", "Luis", "Manuel", "Marcos", "Mariano", "Mario", "Martín", "Mateo", "Mauro",
+    "Miguel", "Nicolás", "Noé", "Octavio", "Orlando", "Oscar", "Pablo", "Patricio", "Pedro", "Rafael",
+    "Raúl", "Raimundo", "Ramiro", "Ricardo", "Roberto", "Rodrigo", "Roque", "Rubén", "Salvador", "Samuel",
+    "Santiago", "Sergio", "Teodoro", "Tomás", "Vicente", "Victor"
+];
+
+const femaleNames = [
+    "Adriana", "Alba", "Alejandra", "Alicia", "Amalia", "Amparo", "Ana", "Andrea", "Angela", "Antonia",
+    "Beatriz", "Berta", "Blanca", "Candela", "Carla", "Carmen", "Carolina", "Catalina", "Celeste", "Clara",
+    "Claudia", "Consuelo", "Cristina", "Daniela", "Dolores", "Elena", "Eliana", "Elisa", "Elsa", "Emilia",
+    "Esperanza", "Estefanía", "Esther", "Eugenia", "Eva", "Fabiola", "Fatima", "Felisa", "Fernanda", "Florencia",
+    "Francisca", "Gabriela", "Gloria", "Graciela", "Guadalupe", "Inés", "Irene", "Isabel", "Isabella", "Jimena",
+    "Josefa", "Julia", "Laura", "Leonor", "Leticia", "Lidia", "Lorena", "Lucía", "Luisa", "Magdalena",
+    "Manuela", "Marina", "Marisol", "María", "Marta", "Mercedes", "Micaela", "Miriam", "Natalia", "Nerea",
+    "Nieves", "Noelia", "Nuria", "Olga", "Paloma", "Patricia", "Paula", "Paz", "Pilar", "Raquel",
+    "Rebeca", "Rocío", "Rosa", "Rosario", "Salomé", "Sandra", "Silvia", "Sofía", "Sonia", "Susana",
+    "Tamara", "Teresa", "Valentina", "Vanesa", "Verónica", "Victoria", "Virginia", "Yolanda", "Zaira"
+];
+
+const surnames = [
+    "Alonso", "Álvarez", "Benítez", "Blanco", "Blesa", "Cabrera", "Calvo", "Cano", "Carmona", "Castillo",
+    "Castro", "Crespo", "Delgado", "Díaz", "Domínguez", "Escudero", "Esteban", "Fernández", "Ferrer", "Flores",
+    "Fuentes", "Gallego", "García", "Garrido", "Gil", "Giménez", "Gómez", "González", "Guerrero", "Gutiérrez",
+    "Hernández", "Herrera", "Hidalgo", "Iglesias", "Jiménez", "López", "Lorenzo", "Marín", "Martínez", "Medina",
+    "Mendoza", "Molina", "Montero", "Montes", "Morales", "Moreno", "Moya", "Navarro", "Nieto", "Núñez",
+    "Ortega", "Ortiz", "Paredes", "Parra", "Pascual", "Pérez", "Prieto", "Ramírez", "Ramos", "Reyes",
+    "Ríos", "Rodríguez", "Romero", "Rubio", "Ruiz", "Sáez", "Sánchez", "Santana", "Santos", "Sanz",
+    "Serrano", "Soler", "Soria", "Soto", "Suárez", "Torres", "Valencia", "Valenzuela", "Vargas", "Vázquez",
+    "Vega", "Velasco", "Vidal", "Vila", "Villanueva", "Villar", "Yáñez", "Zamora", "Zapata", "Zúñiga"
+];
+
+// Usa
+export {
+    maleNames,
+    femaleNames,
+    surnames
+};
